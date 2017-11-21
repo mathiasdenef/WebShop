d99c6903f07ff33b1b22c0f5cf8a000aad2d48db
mathiasdenef <mathiasdenef@hotmail.com>
2017-11-21 23:25:00 +0100

    Added HomeModule and ProductModule. Added routing and bootstrap js plugins

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,38 @@
+import { ProductComponent } from './product/product.component';
+import { HomeModule } from './home/home.module';
+import { HomeComponent } from './home/home.component';
+import { AppBootstrapModule } from './app-bootstrap.module';
+import { ShopModule } from './shop/shop.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { ShopModule } from '../shop/shop.module';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductModule } from './product/product.module';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'product', component: ProductComponent },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: '**', redirectTo: '/home', } // later naar een error pagina veranderen
+];
 
 @NgModule({
   imports: [
-    BrowserModule, ShopModule
+    BrowserModule,
+    AppBootstrapModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false } // <-- debugging purposes only
+    ),
+    // Feature modules
+    ShopModule,
+    HomeModule,
+    ProductModule
   ],
   declarations: [
     AppComponent,
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { ShopModule } from './shop/shop.module';
 
 if (environment.production) {
   enableProdMode();
